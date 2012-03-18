3ce4cb6fda3cfd63cafe749596f9f96f5b992d2b
Luke Young <innoying@innoyings-MacBook-Pro.local>
2012-03-18 10:39:31 -0500

    Added LocalNotification.h

---
+++ LocalNotification.h
@@ -0,0 +1,24 @@
+//
+//  LocalNotification.h
+//
+//  Created by Luke Young (innoying) on 3/18/2012.
+//  Copyright 2012 innoying.com
+//  Released under MIT license
+//
+
+#import <Foundation/Foundation.h>
+
+#ifdef CORDOVA_FRAMEWORK
+#import <Cordova/CDVPlugin.h>
+#else
+#import "CDVPlugin.h"
+#endif
+
+@interface LocalNotification : CDVPlugin {
+}
+
+- (void)addNotification:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options;
+- (void)cancelNotification:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options;
+- (void)cancelAllNotifications:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options;
+
+@end
